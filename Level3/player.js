127cbb6cb18e601ecfa16679ce7a456d8490a86b
Prashant Kessa <prashant.kessa@gmail.com>
2024-10-22 00:07:07 +0200

    models i guess

---
+++ Level3/player.js
@@ -0,0 +1,12 @@
+export const player = {
+    "hollow_knight":{
+        height_diff:150,
+        scene:"hollow_knight/scene.gltf",
+        scaleX:0.5,
+        scaleY:0.5,
+        scaleZ:0.5,
+        positionY:1,
+        positionX:0,
+        positionZ:0
+    }
+};
