80c559e309844a0ed362cff89d2af50ff6f3506f
Prashant Kessa <prashant.kessa@gmail.com>
2024-10-22 17:17:47 +0200

    adjusted hit boxes for each enemy

--- Level3/level3.js
+++ Level3/level3.js
@@ -55,12 +55,12 @@ const loaderObject = new GLTFLoader(loadingManager);
 
 const monsterLoader = new GLTFLoader(loadingManager);
 const currentMonster = getRandomMonster();
-// const currentMonster = monster.anya;
+// const currentMonster = monster.toon_spike;
 
 loaderObject.load(currentMonster.scene, function (gltf) {
     monsterModel = gltf.scene;
     scene.add(monsterModel);
-    monsterModel.position.set(currentMonster.positionX, currentMonster.positionY, currentMonster.positionZ);
+    monsterModel.position.set(currentMonster.positionX, currentMonster.positionY, currentMonster.positionZ-2);
     monsterModel.scale.set(currentMonster.scaleX, currentMonster.scaleY, currentMonster.scaleZ);
     monsterModel.castShadow = true;
 }, undefined, function (error) {
@@ -109,7 +109,23 @@ function restartGame() {
 
     // Reset character and enemy positions
     cube.position.set(0, 1.5, 0); // Reset player position
-    cubeEnemy.position.set(10, 2, 5); // Reset enemy position
+    switch(currentMonster){
+        case monster.tall_monster:
+            cubeEnemy.position.set(10, 2, 5); // Reset enemy position
+            break;
+        case monster.monster_ignan:
+            cubeEnemy.position.set(10, 3, 5); // Reset enemy position
+            break;
+        case monster.toon_spike:
+            cubeEnemy.position.set(10, 4.5, 5); // Reset enemy position
+            break;
+        case monster.anya:
+            cubeEnemy.position.set(10, 1, 5); // Reset enemy position
+            break;
+        default:
+            cubeEnemy.position.set(10, 3, 5); // Reset enemy position
+            break;
+    }
     cubeEnemy.material.color.set(0x040405); // Reset enemy color to original'
     enemyLight.position.copy(cubeEnemy.position); // Reset light position to enemy position
 
@@ -122,6 +138,7 @@ function restartGame() {
 
     // Reset health
     health = maxHealth; // Reset current health to max
+    enemyCurrentHealth = enemyMaxHealth;
 
     // make enemy visible again
     // cubeEnemy.visible = true;
@@ -151,10 +168,44 @@ scene.add(cube);
 cube.visible = false;
 
 // Cube (enemy)
-const geometryEnemy = new THREE.BoxGeometry(1.3, 3, 1.3);
+let geometryEnemy = null;
 const materialEnemy = new THREE.MeshStandardMaterial({ color: 0x040405 });
-const cubeEnemy = new THREE.Mesh(geometryEnemy, materialEnemy);
-cubeEnemy.position.set(10, 2, 5); // Set initial position of the cube
+let cubeEnemy = null;
+let enemyMaxHealth; // Max health of the enemy
+switch(currentMonster){
+    case monster.tall_monster:
+        enemyMaxHealth = 150;
+        geometryEnemy = new THREE.BoxGeometry(1.3, 3, 1.3);
+        cubeEnemy = new THREE.Mesh(geometryEnemy, materialEnemy);
+        cubeEnemy.position.set(10, 2, 5); // Set initial position of the cube
+        break;
+    case monster.monster_ignan:
+        enemyMaxHealth = 200;
+        geometryEnemy = new THREE.BoxGeometry(3, 5, 3);
+        cubeEnemy = new THREE.Mesh(geometryEnemy, materialEnemy);
+        cubeEnemy.position.set(10, 3, 5); // Set initial position of the cube
+        break;
+    case monster.toon_spike:
+        enemyMaxHealth = 200;
+        geometryEnemy = new THREE.BoxGeometry(5.5, 8, 5.5);
+        cubeEnemy = new THREE.Mesh(geometryEnemy, materialEnemy);
+        cubeEnemy.position.set(10, 4.5, 5); // Set initial position of the cubes
+        break;
+    case monster.anya:
+        enemyMaxHealth = 100;
+        geometryEnemy = new THREE.BoxGeometry(1.3, 2, 1.3);
+        cubeEnemy = new THREE.Mesh(geometryEnemy, materialEnemy);
+        cubeEnemy.position.set(10, 1, 5); // Set initial position of the cube
+        break;
+    default:
+        enemyMaxHealth = 100;
+        geometryEnemy = new THREE.BoxGeometry(1.3, 3, 1.3);
+        cubeEnemy = new THREE.Mesh(geometryEnemy, materialEnemy);
+        cubeEnemy.position.set(10, 3, 5); // Set initial position of the cube
+        break;
+}
+
+// const geometryEnemy = new THREE.BoxGeometry(1.3, 3, 1.3);
 scene.add(cubeEnemy);
 cubeEnemy.visible = false;
 
@@ -480,10 +531,27 @@ function animate() {
     // Ensure monsterModel and cubeEnemy are loaded before accessing their properties
     if (monsterModel && cubeEnemy) {
         monsterModel.position.copy(cubeEnemy.position);
-        let temp = cube.rotation*-1;
-        // monsterModel.rotation.copy(cubeEnemy.rotation);
-        monsterModel.rotation.copy(temp);
-        monsterModel.lookAt(cube.position);
+        // let temp = cube.rotation*-1; 
+        monsterModel.rotation.copy(cubeEnemy.rotation);
+        // monsterModel.rotation.copy(temp);
+        // monsterModel.lookAt(cube.position);
+        switch(currentMonster){
+            case monster.tall_monster:
+                monsterModel.position.y = cubeEnemy.position.y - 0;
+                break;
+            case monster.monster_ignan:
+                monsterModel.position.y = cubeEnemy.position.y - 2.5;
+                break;
+            case monster.toon_spike:
+                monsterModel.position.y = cubeEnemy.position.y - 4;
+                break;
+            case monster.anya:
+                monsterModel.position.y = cubeEnemy.position.y - 0.5;
+                break;
+            default:
+                monsterModel.position.y = cubeEnemy.position.y + 1.5;
+                break;
+        }
     }
 
     playerLight.position.set(cube.position.x, cube.position.y + 1.5, cube.position.z);
@@ -583,7 +651,7 @@ const crosshair = new Crosshair(5, 'white');
 
 // Shooting the enemy
 // Enemy health variables
-const enemyMaxHealth = 100; // Max health of the enemy
+
 let enemyCurrentHealth = enemyMaxHealth; // Current health starts at max
 
 let enemyHits = 0; // Initialize hit counter
@@ -721,7 +789,7 @@ function enemyShoot() {
 }
 
 // Health System
-let maxHealth = 100;
+let maxHealth = 100; // Define the maximum health
 let health = maxHealth;
 let loaded = false;
 const damageRate = 1; // Define the damage rate
